ef02b159d773e3058bf0310f4172b1ae89b8815c
”sinanartun” <sinanartun@gmail.com>
2024-07-29 21:43:28 +0200

    up

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 async function setupFaceDetection() {
-    const video = document.getElementById('video');
-    const canvas = document.getElementById('overlay');
-    const context = canvas.getContext('2d');
-  
-    // Load face-api models
-    await faceapi.nets.tinyFaceDetector.loadFromUri('https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights');
-    await faceapi.nets.faceLandmark68Net.loadFromUri('https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights');
-    await faceapi.nets.faceRecognitionNet.loadFromUri('https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights');
-  
-    // Start video stream
-    navigator.mediaDevices.getUserMedia({ video: {} })
-      .then(stream => {
-        video.srcObject = stream;
-      })
-      .catch(err => console.error('Error accessing webcam: ', err));
-  
-    video.addEventListener('play', () => {
-      const displaySize = { width: video.width, height: video.height };
-      faceapi.matchDimensions(canvas, displaySize);
-  
-      setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-      }, 100);
-    });
-  }
-  
-  setupFaceDetection();
-  
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('overlay');
+  const context = canvas.getContext('2d');
+
+  // Load face-api models
+  await faceapi.nets.tinyFaceDetector.loadFromUri('https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/weights');
+  await faceapi.nets.faceLandmark68Net.loadFromUri('https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/weights');
+  await faceapi.nets.faceRecognitionNet.loadFromUri('https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/weights');
+
+  // Start video stream
+  navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(stream => {
+      video.srcObject = stream;
+    })
+    .catch(err => console.error('Error accessing webcam: ', err));
+
+  video.addEventListener('play', () => {
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    }, 100);
+  });
+}
+
+setupFaceDetection();
